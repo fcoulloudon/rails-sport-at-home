b4966b6092f22537056ed4fac3dadbae1d3a1fcd
Mehdi Benzadi <mehdi.benzadi@gmail.com>
2021-04-27 20:49:03 +0200

    Equipment-list branch created - Ajout du equipments_controller.rb

---
+++ app/controllers/equipments_controller.rb
@@ -0,0 +1,25 @@
+class EquipmentsController < ApplicationController
+    def create
+      @rents = Rent.find(params[:rent_id])
+      @equipment = Equipment.new(equipment_params)
+      @equipment.rents = @rents
+      if @equipment.save
+        redirect_to rents_path(@rents)
+      else
+        render 'equipments/show'
+      end
+    end
+  
+    def destroy
+      @equipment = Equipment.find(params[:id])
+      @rents = @equipment.rents
+      @equipment.destroy
+      redirect_to rents_path(@rents)
+    end
+  
+    private
+  
+    def equipment_params
+      params.require(:equipment).permit(:name, :image_url)
+    end
+  end
