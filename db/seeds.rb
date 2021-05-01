fd9e42a7e033e22a5cb205618870649502d11b3f
mathis ducasse <ducasse.mathis@gmail.com>
2021-05-01 16:48:41 +0200

    This is a footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts "Cleaning database"
+User.destroy_all
+
+puts "Creating users"
+10.times do 
+user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, phone:Faker::PhoneNumber.cell_phone_in_e164, password: "12345678", user_type: "Owner")
+end 
+puts "Finished!"
